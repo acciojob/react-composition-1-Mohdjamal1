5f346931b10bd835c627a10d647cc8eb4d8c02a5
Mohdjamal1 <mohdjamalh786@gmail.com>
2023-08-27 22:18:02 +0530

    submitting code-Sun Aug 27 2023 22:17:12 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,16 @@
-
-import React from "react";
+import React,{useState} from "react";
 import './../styles/App.css';
+import Tabs from "./Tabs";
 
 const App = () => {
+  const[data,setData] = useState();
   return (
     <div>
-        {/* Do not remove the main div */}
+      <li onClick={()=>setData("Tab 1")}>Tab 1</li>
+       <li onClick={()=>setData("Tab 2")}>Tab 2</li>
+       <li onClick={()=>setData("Tab 3")}>Tab 3</li>
+
+       <Tabs value={data} />
     </div>
   )
 }
---
+++ src/components/Tabs.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Tabs = (props) => {
+  return <div>This is the content for {props.value}</div>;
+};
+
+export default Tabs;
